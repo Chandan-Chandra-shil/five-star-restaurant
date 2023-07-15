b3868abb5194a929e3c345cdf05f1db231f9d957
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-07-15 23:31:53 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-5xl text-purple-400">hello </h1>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Button</button>
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-accent">Button</button>
+        <button className="btn btn-ghost">Button</button>
+        <button className="btn btn-link">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
